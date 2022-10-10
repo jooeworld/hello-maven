a9b4756e4f5d0219b0f9fbfe5d503c8cf9a3d20c
dhustlerz <jashan.pahwa007@gmail.com>
2022-10-10 16:03:25 -0400

    update maven to spring boot application

--- src/main/java/com/ibt/app/App.java
+++ src/main/java/com/ibt/app/App.java
@@ -1,13 +1,22 @@
 package com.ibt.app;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@SpringBootApplication
+public class App extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
     }
+
+    @GetMapping("/")
+    String helloWorld() {
+        return "Hello World!";
+    }
+
 }
+
---
+++ src/main/java/com/ibt/app/Greeting.java
@@ -0,0 +1,22 @@
+package com.ibt.app;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// Annotation
+@RestController
+
+// Class
+public class Greeting {
+
+    @GetMapping("/hello")
+    // Easy method just to print encouraging and consoling words
+    public String hello() {
+        return "Hello IBT, this is a simple hello message to take care and have a nice day.";
+    }
+
+    @GetMapping("/greet")
+    //Easy method just to print greeting message by saying spring-based applications
+    public String greet() {
+        return "Hello Student, Welcome to IBT learning .";
+    }
+}
